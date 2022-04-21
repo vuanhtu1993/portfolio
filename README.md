440884aaf089517185d0e99d5b442c87bee627a6
Vũ Anh Tú <anhhtus@Anhhtus.local>
2022-04-21 17:27:02 +0700

    3. Add tailwind into project

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### Bước 1: Thêm React Router Dom
 ### Bước 2: Add CSS module + Styled components
+- CSS Module là tính năng của Create-react-app
+- Styled components là thư viện tạo nhanh các component nhỏ tiện lại cho ui
 ### Bước 3: Add CSS Framework phù hợp
+- Chon tailwnid là css framwork cho hệ thống vì tiêu chí chỉ can thiệp css thuần
+- Chứng tỏ rất hiệu quả
+- Các bước thực hiện như sau: https://tailwindcss.com/docs/guides/create-react-app
 ### Bước 4: Cắt HTML trang Home
 ### Bước 5: Responsive trang Home
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import style from './home.module.scss'
-import styled from 'styled-components'
+import style from "./home.module.scss";
+import styled from "styled-components";
 
 const Avatar = styled.div`
-    background: red;
-    height: 100px;
-`
+  background: red;
+  height: 100px;
+`;
 
 const Home = () => {
-    return (
-        <div className={style.container}>
-            Vu anh tus
-            <Avatar/>
-        </div>
-    )
-}
+  return (
+    <div className={style.container}>
+      Vu anh tus
+      <Avatar />
+      <div className="text-3xl font-bold underline">Hello world!</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
